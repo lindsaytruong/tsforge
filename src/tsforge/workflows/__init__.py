87927736140c2fd23630cd66425087d9c66c331c
Lindsay Truong <52465405+lindsaytruong@users.noreply.github.com>
2025-10-08 08:58:05 -0400

    Update __init__.py

--- src/tsforge/workflows/__init__.py
+++ src/tsforge/workflows/__init__.py
@@ -1,36 +1,3 @@
-# """
-# forecast_pkg - Streamlined Forecasting Package
-# ===============================================
-
-# A beginner-friendly forecasting package with unified API and full traceability.
-# """
-
-# from .workflow import Workflow
-# from .recipe import Recipe
-# from .manager import WorkflowManager
-# from .lineage import WorkflowLineage, PipelineResults
-
-# # Version
-# __version__ = "0.1.0"
-
-# # Main exports
-# __all__ = [
-#     "Workflow",
-#     "Recipe",
-#     "WorkflowManager",
-#     "WorkflowLineage",
-#     "PipelineResults",
-# ]
-
-# Backward compatibility (deprecated)
-# from .workflow import (
-#     make_mlf_workflow,
-#     make_sf_workflow,
-#     make_nf_workflow,
-#     make_ensemble,
-#     make_custom_workflow,
-# )
-
 from .baselines import *
 from .manager import *
 from .workflow import *
