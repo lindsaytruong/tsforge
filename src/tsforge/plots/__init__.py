09f54391b1fe6d5bf491b6d3624cfd39347bbd94
Jeff Tackes <tackes@gmail.com>
2025-12-01 16:05:23 -0600

    updated init v3

--- src/tsforge/plots/__init__.py
+++ src/tsforge/plots/__init__.py
@@ -3,8 +3,7 @@ from .plot_cv_results import plot_cv_results
 from .plot_correlation import plot_autocorrelation
 from .plot_calendar_heatmap import plot_calendar_heatmap
 
-
-from .charts import plot_timeseries
+from .charts import plot_timeseries, plot_seasonal
 from .core import THEMES, PALETTE, HIGHLIGHT
 
 __all__ = [
--- src/tsforge/plots/charts/__init__.py
+++ src/tsforge/plots/charts/__init__.py
@@ -3,6 +3,7 @@ Charting functions (user-facing plotting APIs).
 """
 
 from .plot_timeseries import plot_timeseries
+from .plot_seasonal import plot_seasonal
 
 __all__ = [
     "plot_timeseries", "plot_seasonal"
